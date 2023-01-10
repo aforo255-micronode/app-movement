edfeab920755c4f1e84781559c186571e3604372
Jean Flores <jeancarlo_flores@hotmail.com>
2023-01-10 18:50:23 -0500

    add nacos

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// const PORT = process.env.SERVER_PORT_MOVEMENT || 3004
+
+// app.use(express.json())
+// app.use('/api', require('./src/app/routes'))
+
+// app.listen(PORT, () => {
+//     console.log('Application running on port ', PORT)
+// })
+
+const appPromise = require('./src/app/middlewares/configprovider').appPromise
+ 
+appPromise.then(function(app) {
+    const PORT = process.env.SERVER_PORT_MOVEMENT || 3004
+    app.use('/api', require('./src/app/routes'))
+    app.listen(PORT, () => {
+        console.log('Application running on port ', PORT)
+    })
+});
