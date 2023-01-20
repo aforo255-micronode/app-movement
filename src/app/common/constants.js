ca45a141f0841184d986823b19e62c15e4b4657a
Jean Flores <jeancarlo_flores@hotmail.com>
2023-01-20 18:48:36 -0500

    ADD: Github actions

---
+++ src/app/common/constants.js
@@ -0,0 +1,5 @@
+const APP_NAME = "MovementApp"
+
+module.exports = {
+    APP_NAME
+}
--- src/app/repositories/movement.repository.js
+++ src/app/repositories/movement.repository.js
@@ -7,7 +7,7 @@ const movementRepository = {
         return new Promise((resolve, reject) => {
             MongoClient.connect(process.env.DB_MONGO_URI, function (err, db) {
                 if (err) throw err
-                const query = { accountid: accountId }
+                const query = { accountId: accountId }
                 db.db(process.env.DB_MONGO_DATABASE_MOVEMENT).collection("movement").find(query).toArray(function (err, result) {
                     if (err) throw err
                     db.close()
